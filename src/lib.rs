693125dfc65cf0d7c952f885c6cb7a05fbe8f033
Todd Persen <todd.persen@gmail.com>
2017-03-15 00:41:03 -0700

    Fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,15 @@ use std::collections::BTreeMap;
 use bit_vec::BitVec;
 
 struct Series {
-    t0: u64,
-    tn: u64,
-    vn: f64,
-    bv: BitVec,
+    t0: u64,        // first timestamp in this block
+    tn: u64,        // latest timestamp in this block
+    td: u64,        // the timestamp delta?
+    vn: f64,        // latest value in this block
+    bv: BitVec,     // the stream of bits
 
     leading: u8,
     trailing: u8,
     finished: bool,
-
-    td: u64
 }
 
 impl Series {
@@ -21,30 +20,30 @@ impl Series {
         Series {
             t0: 0,
             tn: 0,
+            td: 0,
             vn: 0f64,
             bv: BitVec::new(),
             leading: 0,
             trailing: 0,
             finished: false,
-            td: 0
         }
     }
 
     pub fn push(&mut self, t: u64, v: f64) {
-
         // is this the first point?
         if self.tn == 0 {
-            self.tn = t;
-            self.vn = v;
-            self.td = t;
+            self.tn = t.clone();
+            self.vn = v.clone();
+            self.td = t.clone();
 
-            self.push_bits(t, 14);
+            self.push_bits(v, 64);
         }
     }
 
-    fn push_bits(&mut self, v: u64, n: u8) {
-        for x in 0..n {
-            self.bv.push(v && 2 << n == 1);
+    fn push_bits(&mut self, v: f64, n: u8) {
+        let b64 = unsafe { std::mem::transmute::<f64, u64>(v) };
+        for x in n-1..0 {
+            self.bv.push(b64 & (1 << x) > 0);
         }
     }
 }
@@ -62,10 +61,9 @@ mod tests {
 
     #[test]
     fn test_push_series() {
-        let s = Series::new();
+        let mut s = Series::new();
 
         s.push(999, 3.14);
-        assert_eq!(s.t0, 999);
         assert_eq!(s.tn, 999);
         assert_eq!(s.td, 999);
         assert_eq!(s.vn, 3.14);
