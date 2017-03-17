d6f3c7e40757e4c39730850b925c8e5eb40515bb
Todd Persen <todd.persen@gmail.com>
2017-03-16 17:30:24 -0700

    Start sorting out control codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 extern crate bit_vec;
 
-use std::collections::BTreeMap;
+//use std::collections::BTreeMap;
 use bit_vec::BitVec;
 
 struct Series {
     t0: u64,        // beginning timestamp of this block (
     vprev: u64,     // previous value, stored bitwise
+    lz: u8,         // current number of leading zero nibbles (4-bits)
     bv: BitVec,     // the stream of bits
     finished: bool, // is this block complete?
 }
@@ -15,6 +16,7 @@ impl Series {
         Series {
             t0: t0,
             vprev: 0,
+            lz: 0xff,
             bv: BitVec::new(),
             finished: false,
         }
@@ -25,23 +27,47 @@ impl Series {
         let bits = unsafe { std::mem::transmute::<f64,u64>(v) };
 
         // if this is the first point, write the entire value
-        if bv.len() == 0 {
-            self.append_bits(v, 64);
-
+        if self.bv.len() == 0 {
+            self.vprev = bits;
+            self.append_bits(bits, 64);
             return;
         }
 
         let xor = self.vprev ^ bits;
-
         self.vprev = bits;
+
+        // control code 00 = repeated point
+        if bits == 0 {
+            self.append_bits(0b00, 2);
+            return;
+        }
+
+        let lz = xor.leading_zeros() as u8;
+        let zero_nibbles = lz % 4;
+        let remaining_bits = 64 - (4 * zero_nibbles);
+
+        // if we haven't recorded the leading zeros yet, do that now
+        if self.lz == 0xff {
+            self.lz = zero_nibbles;
+        }
+
+        // control code 10 = continue using current lz
+        self.append_bits(0b10, 2);
+        self.append_bits(bits, remaining_bits);
+
+        // control code 01 = null value
+        // TODO: this needs to get handled outside of this function
+
+        // control code 11 = reset lz
+        // TODO: decide if it's efficient to reset
+
+        return;
     }
 
-    fn append_bits(&mut self, v: u64, n: u8) {
-        let bits = unsafe { std::mem::transmute::<f64,u64>(v) };
-
+    fn append_bits(&mut self, value: u64, num_bits: u8) {
         // TODO: bail out if n > 64, otherwise we'll overflow
-        for x in n-1..0 {
-            self.bv.push(bits & (2 << n) > 0);
+        for x in num_bits-1..0 {
+            self.bv.push(value & (2 << x) > 0);
         }
     }
 }
@@ -49,6 +75,10 @@ impl Series {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn bits(v: f64) -> u64 {
+        unsafe { std::mem::transmute::<f64,u64>(v) }
+    }
 
     #[test]
     fn test_new_series() {
@@ -62,9 +92,8 @@ mod tests {
     fn test_push_series() {
         let mut s = Series::new(0);
 
-        s.push(999, 3.14);
-        assert_eq!(s.tn, 999);
-        assert_eq!(s.vn, 3.14);
+        s.append(3.14);
+        assert_eq!(s.vprev, bits(3.14));
     }
 }
 
